655ac51e286ca6316740c4ef0c7edc55d27618b3
NagababuV <nagababu.vegisetti@gmail.com>
2024-06-23 20:44:15 +0530

    first commit

---
+++ src/main/java/com/example/student/controller/StudentController.java
@@ -0,0 +1,62 @@
+package com.example.student.controller;
+
+import com.example.student.exception.StudentNotFoundException;
+import com.example.student.model.Student;
+import com.example.student.repository.StudentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+@RequestMapping("/student")
+public class StudentController {
+
+
+    @Autowired
+    private StudentRepo studentRepo;
+    @PostMapping("/add")
+    public Student newStudent(@RequestBody Student newStudent){
+        return studentRepo.save(newStudent);
+
+
+    }
+
+    @GetMapping("/getall")
+    public List<Student> getAll(){
+
+        return studentRepo.findAll();
+
+    }
+
+    @GetMapping("/findById/{id}")
+    public Student  getStudentById(@PathVariable Long id){
+        return studentRepo.findById(id)
+        .orElseThrow(()->new StudentNotFoundException(id));
+
+    }
+    @PutMapping("/update/{id}")
+    Student updateStudent(@RequestBody Student newStudent ,@PathVariable Long id){
+        return studentRepo.findById(id)
+                .map(student -> {
+                    student.setName(newStudent.getName());
+                    student.setRegistrationNo(newStudent.getRegistrationNo());
+                    student.setMailId(newStudent.getMailId());
+                    return  studentRepo.save(student);
+                }).orElseThrow(()-> new StudentNotFoundException(id));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteById(@PathVariable Long id){
+
+        if(!studentRepo.existsById(id)){
+            throw new StudentNotFoundException(id);
+        }
+        studentRepo.deleteById(id);
+
+        return "Entered id details has been deleted";
+
+
+    }
+}
---
+++ src/main/java/com/example/student/model/Student.java
@@ -0,0 +1,54 @@
+package com.example.student.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private int registrationNo;
+    private String mailId;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRegistrationNo() {
+        return registrationNo;
+    }
+
+    public void setRegistrationNo(int registrationNo) {
+        this.registrationNo = registrationNo;
+    }
+
+    public String getMailId() {
+        return mailId;
+    }
+
+    public void setMailId(String mailId) {
+        this.mailId = mailId;
+    }
+
+
+
+
+}
